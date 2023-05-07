d4d83b4a83c865eeb09b20241a3c9ec193f5fe44
Jose <jluisrr62@educastur.es>
2023-05-07 12:41:51 +0200

    añadida operacion al servicio de deposito

--- src/main/java/com/fleming/bancodelibros/services/impl/DepositoServiceImpl.java
+++ src/main/java/com/fleming/bancodelibros/services/impl/DepositoServiceImpl.java
@@ -1,18 +1,30 @@
 package com.fleming.bancodelibros.services.impl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fleming.bancodelibros.modelo.Deposito;
+import com.fleming.bancodelibros.repos.DepositoRepository;
 import com.fleming.bancodelibros.services.DepositoService;
 
 public class DepositoServiceImpl implements DepositoService{
 	
+	@Autowired
+	DepositoRepository depositoRepo;
+	
 	@Override
 	public List<LocalDateTime> getFechasPorAdminId(Long adminId) {
+		List<LocalDateTime> fechas = new ArrayList<>();
 		
+		for (Deposito depo : depositoRepo.findAll()) {
+			if(depo.getAdmin().getId() == adminId) 
+				fechas.add(depo.getId().getFecha());
+		}
 		
-		
-		return null;
+		return fechas;
 	}
 
 }
